fc70bd7401d0ce9dee3e3f67b13a5317278ef6ec
Rizaldy2021 <123721616+Rizaldy2021@users.noreply.github.com>
2024-12-17 15:55:53 +0700

    Fix Nothing

--- resources/views/user/userDashboard.blade.php
+++ resources/views/user/userDashboard.blade.php
@@ -4,7 +4,7 @@
     <div>
         <x-slot name="header">
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                {{ __('File Explorer') }}
+                {{ __('File Explorer') }} 
             </h2>
         </x-slot>
         <div class="bg-white w-full p-4 h-full" id="file-explorer">
