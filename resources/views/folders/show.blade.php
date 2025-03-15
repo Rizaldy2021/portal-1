d068ac138857fdc1749c9bfb17bcfa39ee12660d
Rizaldy653 <sky.limit978@gmail.com>
2025-03-15 21:11:50 +0700

    Fixed layout

--- resources/views/folders/show.blade.php
+++ resources/views/folders/show.blade.php
@@ -6,17 +6,17 @@
         <div class="flex flex-col gap-10">
             <div class="parent-folder">
                 <h2 class="text-gray-600">Folder</h2>
-                <div class="bg-white w-full py-2 gap-4 flex flex-wrap">
+                <div class="bg-white w-full py-2 gap-4 grid grid-cols-2 lg:grid-cols-4 xl:grid-cols-6">
                     @foreach ($folders as $currentFolder)
-                    <x-folderCard :folder="$currentFolder">{{ $currentFolder->name }}</x-folderCard>
+                        <x-folderCard :folder="$currentFolder">{{ $currentFolder->name }}</x-folderCard>
                     @endforeach
                 </div>
             </div>
             <div>
                 <h2 class="text-gray-600">Files</h2>
-                <div class="bg-white w-full py-2 gap-4 flex flex-wrap">
+                <div class="bg-white w-full py-2 gap-4 grid grid-cols-2 lg:grid-cols-4 xl:grid-cols-6">
                     @foreach ($folder->files as $file)
-                    <x-fileCard :file="$file">{{ $file->name }}</x-fileCard>
+                        <x-fileCard :file="$file">{{ $file->name }}</x-fileCard>
                     @endforeach
                 </div>
             </div>
